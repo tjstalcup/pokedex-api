75772aa0daacf6c5dcab3305b68941056712dab2
TJ Stalcup <tstalcup@celerity.com>
2019-06-13 17:58:24 -0500

    Updates from Week5

--- server.js
+++ server.js
@@ -1,15 +1,60 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config()
 
 const app = express();
+const PORT = 8000;
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+const POKEDEX = require('./pokedex.json');
+
 
 app.use(morgan('dev'));
+app.use(cors());
+app.use(helmet());
+app.use(validateBearerToken);
 
-app.use((req,res)=>{
-    res.send('Hello World');
-});
+app.get('/types', handleGetTypes);
+app.get('/pokemon', handleGetPokemon);
 
-const PORT = 8000;
+
+
+
+function validateBearerToken(req, res, next){ 
+    const authToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN;
+
+    if(!authToken || authToken.split(" ")[1] !== apiToken){
+        return res.status(401).json({error: 'Unauthorized Request'});
+    }
+    
+    next();
+}
+
+function handleGetTypes(req,res){
+    res.json(validTypes);
+}
+
+function handleGetPokemon(req,res){
+    let response = POKEDEX.pokemon;
+
+    console.log(req.query);
+
+    if(req.query.name){
+        response = response.filter(pokemon => {
+            return pokemon.name.toLowerCase().includes(req.query.name.toLowerCase());
+        });
+    }
+
+    if(req.query.type){
+        response = response.filter(pokemon=>{
+            return pokemon.type.includes(req.query.type);
+        });
+    }
+
+    res.json(response);
+}
 
 app.listen(PORT, ()=> {
     console.log(`Server is running at http://localhost:${PORT}`);
